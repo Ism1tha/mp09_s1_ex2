4c5ef13493a051e884065327347434e88668245d
Ism1tha <xgalan@hotmail.com>
2024-10-15 17:20:40 +0200

    Tailwind js and add tailwind to layout main

--- src/app/Layouts/MainLayout.php
+++ src/app/Layouts/MainLayout.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $_ENV['APP_NAME'] . ' - ' . $title ?></title>
     <link rel="stylesheet" href="/public/assets/css/main.css">
+    <script src="/public/assets/js/tailwind.js"></script>
     <script src="/public/assets/js/main.js"></script>
 </head>
 
