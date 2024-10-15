b0e86fd3023b1083e962ff2d9c1d5dc6ab76b05e
Ism1tha <xgalan@hotmail.com>
2024-10-15 17:29:43 +0200

    MainLayout now using Tailwind + remove classes used in MainLayout from CSS file

--- src/app/Layouts/MainLayout.php
+++ src/app/Layouts/MainLayout.php
@@ -11,19 +11,19 @@
 </head>
 
 <body>
-    <header>
-        <div class="title"><?= $title ?></div>
+    <header class="bg-gray-800 text-white md-flex md-justify-between md-items-center px-4 py-4">
+        <div class="text-2xl"><?= $title ?></div>
         <?php if ($return_path != null): ?>
-            <a class="return-btn" href="<?= $return_path ?>">
+            <a class="inline-block bg-blue-500 px-4 py-2 rounded w-full md:w-auto" href="<?= $return_path ?>">
                 Regresar
-                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="white" d="M19 7v4H5.83l3.58-3.59L8 6l-6 6l6 6l1.41-1.41L5.83 13H21V7z"/></svg>
+                <svg class="inline" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="white" d="M19 7v4H5.83l3.58-3.59L8 6l-6 6l6 6l1.41-1.41L5.83 13H21V7z"/></svg>
             </a>
         <?php endif; ?>
     </header>
-    <div class="container">
+    <div class="w-full  p-4 md:w-3/4 md:mx-auto">
         <?= $content ?>
     </div>
-    <footer>
+    <footer class="bg-gray-800 text-white text-center py-8">
         <p>Método factorial: <?= $_ENV['CALCULATOR_NUMERIC_FACTORIAL_METHOD'] == 0 ? 'Recursivo' : 'Iterativo' ?></p>
         <p>© <?= date('Y') ?> - <?= $_ENV['APP_NAME'] ?> - <?= $_ENV['APP_AUTHOR'] ?></p>
     </footer>
